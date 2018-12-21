b989af7ca0abc017855df154eeab1739d753dce0
Dante Spencer <dantespe@umich.edu>
2018-12-20 20:06:52 -0500

    Settings works the same locally and on GCP

--- settings.py
+++ settings.py
@@ -1,30 +1,29 @@
-try:
-    from google.appengine.ext import ndb
+from google.appengine.ext import ndb
+import os
 
-    class Settings(ndb.Model):
-      name = ndb.StringProperty()
-      value = ndb.StringProperty()
+class Settings(ndb.Model):
+  name = ndb.StringProperty()
+  value = ndb.StringProperty()
 
-      @staticmethod
-      def get(name):
-        NOT_SET_VALUE = "NOT SET"
+  @staticmethod
+  def get(name):
+    NOT_SET_VALUE = "NOT SET"
+
+    try:
         retval = Settings.query(Settings.name == name).get()
-        if not retval:
-          retval = Settings()
-          retval.name = name
-          retval.value = NOT_SET_VALUE
-          retval.put()
-        if retval.value == NOT_SET_VALUE:
-          raise Exception(('Setting %s not found in the database. A placeholder ' +
-            'record has been created. Go to the Developers Console for your app ' +
-            'in App Engine, look up the Settings record with name=%s and enter ' +
-            'its value in that record\'s value field.') % (name, name))
-        return retval.value
+    except:
+        return os.environ.get(name, None)
 
-except:
-    import os
-    
-    class Settings():
-        @staticmethod
-        def get(name):
-            return os.environ.get(name, "NOT SET")
+    if not retval:
+      retval = Settings()
+      retval.name = name
+      retval.value = NOT_SET_VALUE
+      retval.put()
+
+    if retval.value == NOT_SET_VALUE:
+      raise Exception(('Setting %s not found in the database. A placeholder ' +
+        'record has been created. Go to the Developers Console for your app ' +
+        'in App Engine, look up the Settings record with name=%s and enter ' +
+        'its value in that record\'s value field.') % (name, name))
+
+    return retval.value
